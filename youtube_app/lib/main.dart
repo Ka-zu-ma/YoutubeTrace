a70423b9a4de348b9da3f49c694a2af54426bcbf
Ka-zu-ma <km.km.kzkz00@gmail.com>
2022-01-26 03:08:07 +0900

    ThemeData変更

--- youtube_app/lib/main.dart
+++ youtube_app/lib/main.dart
@@ -8,6 +8,9 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.light().copyWith(primary:  Colors.white,),
+      ),
         home:Scaffold(
           appBar: AppBar(
             title: const Text('Youtubeアプリ'),
