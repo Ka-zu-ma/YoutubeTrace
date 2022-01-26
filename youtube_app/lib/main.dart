3fd1a8d81151a630ce15c7546f8e41582b15488b
Ka-zu-ma <km.km.kzkz00@gmail.com>
2022-01-26 16:54:40 +0900

    appBarのactionsにアイコンセット

--- youtube_app/lib/main.dart
+++ youtube_app/lib/main.dart
@@ -15,6 +15,26 @@ class App extends StatelessWidget {
           appBar: AppBar(
             leading: Icon(Icons.videocam, color: Colors.black,),
             title: const Text('Youtubeアプリ',style: TextStyle(color: Colors.black),),
+            actions: [
+              SizedBox(
+                width: 44,
+                child: TextButton(
+                  onPressed: (){},
+                  child: Icon(Icons.search),
+                  style: ButtonStyle(
+                      foregroundColor: MaterialStateProperty.all<Color>(Colors.grey)),
+                ),
+              ),
+              SizedBox(
+                width: 44,
+                child: TextButton(
+                    onPressed: (){},
+                    child: Icon(Icons.more_vert),
+                    style: ButtonStyle(
+                        foregroundColor: MaterialStateProperty.all<Color>(Colors.grey)),
+                ),
+              )
+            ],
           ),
           body: Container(),
         ),
